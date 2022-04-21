199fab326537b6536f9e0ec7a592adc0b80cc919
Tony Chao <tchao@mail.usf.edu>
2022-04-21 19:14:44 -0400

    Add a campground author authorization middleware and export all middleware in campground routes

--- middleware.js
+++ middleware.js
@@ -1,7 +1,33 @@
+const Campground = require('./models/campground');
+const { campgroundSchema } = require('./schemas');
+const ExpressError = require('./utilities/ExpressError');
+
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
         req.flash('error', 'You must be logged in');
         return res.redirect('/login');
+    }
+
+    next();
+}
+
+module.exports.validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+
+    if (error) {
+        const errorMessage = error.details.map(el => el.message).join(',');
+        throw new ExpressError(errorMessage, 400);
+    }
+
+    next();
+}
+
+module.exports.validateAuthor = async (req, res, next) => {
+    const campground = await Campground.findById(req.params.id);
+        
+    if (!campground.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that!');
+        return res.redirect(`/campgrounds/${campground._id}`);
     }
 
     next();
