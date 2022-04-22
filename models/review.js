98bc697881e0327263c449973691d379e203ead3
Tony Chao <tchao@mail.usf.edu>
2022-04-21 22:06:22 -0400

    Update the review schema with a relationship with a user

--- models/review.js
+++ models/review.js
@@ -3,7 +3,11 @@ const Schema = mongoose.Schema;
 
 const reviewSchema = new mongoose.Schema({
     content: String,
-    rating: Number
+    rating: Number,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
 });
 
 const Review = mongoose.model("Review", reviewSchema);
