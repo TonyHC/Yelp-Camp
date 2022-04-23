e2e271a526626fe4611ad67bfb3ab43611fdd4b0
Tony Chao <tchao@mail.usf.edu>
2022-04-22 23:27:24 -0400

    Add image upload using multer and cloudinary on certain campground routes
    In addition, update the creating new campground template to handle image uploading.
    
    Lastly, add new template to handle uploading more images.

--- routes/campground.js
+++ routes/campground.js
@@ -4,18 +4,23 @@ const catchAsyncError = require('../utilities/catchAsyncError');
 const { isLoggedIn } = require('../middleware');
 const { validateAuthor, validateCampground } = require('../middleware');
 const campgrounds = require('../controllers/campgrounds');
+const multer = require('multer');
+const { storage } = require('../cloudinary/config');
+const upload = multer({ storage })
 
 router.route('/')
     .get(catchAsyncError(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsyncError(campgrounds.createCampground));
+    .post(isLoggedIn, upload.array('image'), validateCampground , catchAsyncError(campgrounds.createCampground));
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
 router.route('/:id')
     .get(catchAsyncError(campgrounds.showCampground))
-    .put(validateCampground, isLoggedIn, validateAuthor, catchAsyncError(campgrounds.updateCampground))
+    .patch(isLoggedIn, validateAuthor, upload.array('image'), validateCampground, catchAsyncError(campgrounds.updateCampground))
     .delete(isLoggedIn, validateAuthor, catchAsyncError(campgrounds.deleteCampground));
 
 router.get('/:id/edit', isLoggedIn, validateAuthor, catchAsyncError(campgrounds.renderEditForm));
+
+router.get('/:id/imageUpload', isLoggedIn, validateAuthor, catchAsyncError(campgrounds.renderImageUploadForm));
 
 module.exports = router;
